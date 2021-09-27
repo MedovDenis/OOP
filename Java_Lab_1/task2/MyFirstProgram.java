c428074acb11865c22417cd2024c2b21ae4f2a5b
Medov <38776367+MedovDenis@users.noreply.github.com>
2021-09-27 11:00:05 +0400

    Java Lab1 and Lab2

---
+++ Java_Lab_1/task2/MyFirstProgram.java
@@ -0,0 +1,8 @@
+// javac MyFirstProgram.java
+// java MyFirstClass
+
+class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("Медов");
+    }
+}
---
+++ Java_Lab_1/task3/MyFirstProgram.java
@@ -0,0 +1,10 @@
+// javac MyFirstProgram.java
+// java MyFirstClass M e d o v
+
+class MyFirstClass {
+    public static void main(String[] args) {
+		for(int i = 0; i < args.length; i++){
+			System.out.println(args[i]);
+		}
+    }
+}
