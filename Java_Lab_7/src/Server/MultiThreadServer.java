38046cf2a77fa426267a757f0fcb2118e0fa99f1
Medov <38776367+MedovDenis@users.noreply.github.com>
2021-11-27 11:09:12 +0300

    Lab7

--- Java_Lab_7/src/Server/MultiThreadServer.java
+++ Java_Lab_7/src/Server/MultiThreadServer.java
@@ -1,11 +1,7 @@
 package Server;
 
-import Interface.Transport;
-import Transport.*;
-
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 import java.util.concurrent.*;
 
 public class MultiThreadServer {
--- new/Java_Lab_7/src/Exception/ModelPriceOutOfBoundsException.java
+++
@@ -1,7 +0,0 @@
-package Exception;
-
-public class ModelPriceOutOfBoundsException extends RuntimeException{
-    public ModelPriceOutOfBoundsException() {
-        super ("Задание неверной цены модели");
-    }
-}
